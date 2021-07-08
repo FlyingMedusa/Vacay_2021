fd0232111439539d7cada1b79fb298b18ed39e49
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-08 21:39:21 +0200

    background boxes

---
+++ jsBoxes.js
@@ -0,0 +1,14 @@
+function changeBoxPlum(){
+    document.body.style.backgroundColor="plum";
+    document.getElementById('bcolor').innerHTML="Current background color: plum"
+}
+
+function changeBoxCyan(){
+    document.body.style.backgroundColor="darkCyan";
+    document.getElementById('bcolor').innerHTML="Current background color: dark cyan"
+}
+
+function changeBoxOrchid(){
+    document.body.style.backgroundColor="darkorchid";
+    document.getElementById('bcolor').innerHTML="Current background color: dark orchid"
+}
