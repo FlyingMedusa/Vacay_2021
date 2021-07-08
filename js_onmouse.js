ba6b706c019dff600d715f7abf896483275ab5b0
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-08 20:55:47 +0200

    playing with js

---
+++ js_onmouse.js
@@ -0,0 +1,12 @@
+function changeBoxOne(){
+    document.getElementById('boxOne').style.backgroundColor="orange";
+  }
+  
+  function changeBoxTwo() {
+    document.getElementById('boxTwo').style.backgroundColor="green";
+    document.body.style.backgroundColor="pink";
+  }
+  
+  function changeText() {
+    document.getElementById('para').innerHTML="you have changed the text!"
+  }
