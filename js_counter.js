c4ecf28e13ffee43eac97dd0d05ea0caa9909d55
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-09 22:52:27 +0200

    js counter

---
+++ js_counter.js
@@ -0,0 +1,11 @@
+var value = 0;
+
+function addOne(){
+  value += 1;
+  document.getElementById('number').innerHTML=value;
+}
+
+function subtractOne(){
+    value -= 1;
+    document.getElementById('number').innerHTML=value;
+  }
