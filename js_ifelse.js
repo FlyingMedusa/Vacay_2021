8e3e66f8018600a28bf04b5efcd1e2306d4d308e
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-10 22:09:45 +0200

    basics js if else

---
+++ js_ifelse.js
@@ -0,0 +1,20 @@
+function ifElse(){
+    var personAge = 20;
+    if (personAge > 17) {
+        alert("You can drink in Poland");
+    } else {
+        alert("You are too young!")
+    }
+    
+}
+
+function ifElse2(){
+    var correctPassword = "myPass123";
+    var userAttempt = "pass?";
+    if (correctPassword == userAttempt) {
+        alert("The password is correct");
+    } else {
+        alert("Access denied")
+    }
+    
+}
