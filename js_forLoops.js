502fcce6a06a568badf1705da72baa491c8ddb0a
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-12 10:28:05 +0200

    for loops improved

--- js_forLoops.js
+++ js_forLoops.js
@@ -1,7 +1,9 @@
 var myCars = ["BMW", "Volvo", "Saab", "Ford", "Fiat","Audi"]
-
+var para = '';
 
 for (i = 0; i < myCars.length; i++) {
-    document.getElementById("allCars").innerHTML+=("<br>"+myCars[i]);
+    para += (myCars[i] + "<br>");
   }
+
+  document.getElementById("allCars").innerHTML=para;
   
