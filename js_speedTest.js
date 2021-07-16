2badd54cf49c7e5f7c1ac46076ed446b3c0da74d
Marta Sleboda <sleboda.m98@gmail.com>
2021-07-16 21:05:58 +0200

    finished speed test

--- js_speedTest.js
+++ js_speedTest.js
@@ -8,11 +8,28 @@ function disappear() {
     clicked = Date.now()
     var reactionTime = (clicked-start)/1000;
     alert(reactionTime + " seconds.")
-    setTimeout(appear, 2000);
+    var randomDelay = (Math.random()*3)*1000
+    setTimeout(appear, randomDelay);
     
 }
 
 function appear() {
+    var randomTop = Math.random()*400;
+    var randomLeft = Math.random()*600;
+    var randomHeight = Math.random()*200+10;
+    var randomWidth = Math.random()*200+10;
+    var randomCurve = Math.random();
+    var randomColor = "#" + ((1<<24)*Math.random() | 0).toString(16);
+    if (randomCurve < 0.5) {
+        document.getElementById('box').style.borderRadius=25+'px';
+    } else {
+        document.getElementById('box').style.borderRadius=0+'px';
+    }
+    document.getElementById('box').style.top=randomTop+'px';
+    document.getElementById('box').style.left=randomLeft+'px';
+    document.getElementById('box').style.height=randomHeight+'px';
+    document.getElementById('box').style.width=randomWidth+'px';
     document.getElementById('box').style.display="block";
+    document.getElementById('box').style.backgroundColor=randomColor;
     start=Date.now()
 }
