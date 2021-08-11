1c78a413e0651051b71e267b416d4963a898031d
Marta Sleboda <sleboda.m98@gmail.com>
2021-08-11 21:12:06 +0200

    numbers

---
+++ js_sandbox05.js
@@ -0,0 +1,20 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 % num2;
+
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(2, 22, 3, 54, 1, -3, 63);
+
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
