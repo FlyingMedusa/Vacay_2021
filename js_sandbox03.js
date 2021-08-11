5913f648defb985c937572a4f013f8d9e5ac268c
Marta Sleboda <sleboda.m98@gmail.com>
2021-08-11 19:10:10 +0200

    variables

---
+++ js_sandbox03.js
@@ -0,0 +1,26 @@
+const name = 'Sleboda Marta';
+const age = 45;
+const hasKids = true;
+const car = null;
+let test;
+const sym = Symbol();
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof hasKids);
+console.log(typeof car);
+console.log(typeof test);
+console.log(typeof sym);
+
+const hobbies = ['movies', 'music'];
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+
+console.log(today);
+
+console.log(typeof hobbies);
+console.log(typeof address);
+console.log(typeof today);
