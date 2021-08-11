1cd098ec063b91961572db69c5b7184ea8828c46
Marta Sleboda <sleboda.m98@gmail.com>
2021-08-11 16:26:09 +0200

    variables

---
+++ js_sandbox02.js
@@ -0,0 +1,24 @@
+var name = 'Marta Sleboda';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+let newName = 'Marta Sleboda';
+console.log(newName);
+
+const constName = 'Johnny';
+// Cannot reassign
+
+const dog = {
+    name: 'Sara',
+    age: 3
+}
+dog.name = 'Teddy';
+console.log(dog);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
